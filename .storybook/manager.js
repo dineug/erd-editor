8e4c217a91d0e6a0dbcf51a654dfd3c0bcfcb2b5
dineug <dineug2@gmail.com>
2020-07-03 22:21:54 +0900

    add: storybook dark theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,1 +1,8 @@
+import { addons } from "@storybook/addons";
+import { themes } from "@storybook/theming";
+
+addons.setConfig({
+  theme: themes.dark,
+});
+
 window.STORYBOOK_GA_ID = "UA-131336352-5";
