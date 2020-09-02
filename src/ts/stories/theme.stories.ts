20186c3f6ec9027d8f75bb4450f3fd83fb1d99fe
dineug <dineug2@gmail.com>
2020-09-02 22:55:17 +0900

    change: theme stories

--- src/ts/stories/theme.stories.ts
+++ src/ts/stories/theme.stories.ts
@@ -24,7 +24,26 @@ stories.add(
 
     style.innerText = `
       :root {
-        --vuerd-theme-canvas: #2f4e44;
+        --vuerd-theme-canvas: #d7d7d7;
+        --vuerd-theme-table: #e6e6e6;
+        --vuerd-theme-table-active: #ebb692;
+        --vuerd-theme-focus: #ff5600;
+        --vuerd-theme-key-pk: #B4B400;
+        --vuerd-theme-key-fk: #dda8b1;
+        --vuerd-theme-key-pfk: #60b9c4;
+        --vuerd-theme-font: #5d5d5d;
+        --vuerd-theme-font-active: black;
+        --vuerd-theme-font-placeholder: #929292;
+        --vuerd-theme-contextmenu: #e6e6e6;
+        --vuerd-theme-contextmenu-active: #c7c2be;
+        --vuerd-theme-edit: #003ef8;
+        --vuerd-theme-column-select: #dcd5d0;
+        --vuerd-theme-column-active: #c8d6f7;
+        --vuerd-theme-minimap-shadow: #5d5d5d;
+        --vuerd-theme-scrollbar-thumb: #929292;
+        --vuerd-theme-scrollbar-thumb-active: #5d5d5d;
+        --vuerd-theme-menubar: white;
+        --vuerd-theme-visualization: #d7d7d7;
       }
     `;
 
