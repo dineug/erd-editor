b5a83cc9438b0cd43ad6403da68d8cf17beac6d2
dineug <dineug2@gmail.com>
2021-01-16 19:29:14 +0900

    fix: dependencies

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   plugins: ["@snowpack/plugin-typescript"],
-  installOptions: {},
-  devOptions: {
-    port: 8080,
-    fallback: "index.html",
-    open: "default",
-    output: "dashboard",
-    hostname: "localhost",
-    hmr: true,
-    hmrErrorOverlay: true,
-  },
-  buildOptions: {},
   mount: {
     public: "/",
     src: "/_dist_",
