52a1a85bf265796f7e0e59b60e411aa36abe18ea
dineug <dineug2@gmail.com>
2021-02-18 02:39:51 +0900

    fix: useRenderTrigger

--- src/core/hooks/renderTrigger.hook.ts
+++ src/core/hooks/renderTrigger.hook.ts
@@ -1,12 +1,16 @@
-import { observable, beforeUpdate, firstUpdated } from '@dineug/lit-observable';
+import {
+  observable,
+  beforeFirstUpdate,
+  beforeUpdate,
+} from '@dineug/lit-observable';
 
 export function useRenderTrigger() {
   const state = observable({ count: 0 });
 
   const renderTrigger = () => state.count++;
 
+  beforeFirstUpdate(() => state.count);
   beforeUpdate(() => state.count);
-  firstUpdated(() => state.count);
 
   return {
     renderTrigger,
