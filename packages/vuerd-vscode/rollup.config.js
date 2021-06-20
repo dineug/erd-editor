e5e1d30f815bb7b4c8026dbdd4935aeb664a35fc
dineug <dineug2@gmail.com>
2021-06-21 02:59:28 +0900

    fix: vscode build filesize

--- packages/vuerd-vscode/rollup.config.js
+++ packages/vuerd-vscode/rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import filesize from 'rollup-plugin-filesize';
 
 import pkg from './package.json';
 
@@ -35,5 +36,12 @@ export default {
     'http2',
     'net',
   ],
-  plugins: [resolve(), commonjs(), typescript()],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    filesize({
+      showBrotliSize: true,
+    }),
+  ],
 };
