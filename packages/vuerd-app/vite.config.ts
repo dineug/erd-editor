8b670c6d3c6eadbc2f1ea28b8e0ade623bdc6274
dineug <dineug2@gmail.com>
2021-09-25 13:50:49 +0900

    fix: [vuerd-app] build

--- packages/vuerd-app/vite.config.ts
+++ packages/vuerd-app/vite.config.ts
@@ -8,89 +8,96 @@ import WindiCSS from 'vite-plugin-windicss';
 const resolvePath = (p: string) => path.resolve(__dirname, `./${p}`);
 const customElements = ['erd-editor'];
 
-export default defineConfig({
-  envDir: resolvePath('environment'),
-  resolve: {
-    alias: {
-      '@': resolvePath('src'),
+export default ({ mode }) => {
+  const isSub = mode === 'sub';
+
+  return defineConfig({
+    envDir: resolvePath('environment'),
+    resolve: {
+      alias: {
+        '@': resolvePath('src'),
+      },
     },
-  },
-  plugins: [
-    WindiCSS(),
-    vueJsx(),
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: tag => customElements.includes(tag),
+    build: {
+      outDir: isSub ? '../../../docs' : 'dist',
+    },
+    server: {
+      open: true,
+    },
+    plugins: [
+      WindiCSS(),
+      vueJsx(),
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: tag => customElements.includes(tag),
+          },
         },
-      },
-    }),
-    VitePWA({
-      includeAssets: [
-        'favicon.svg',
-        'favicon.ico',
-        'robots.txt',
-        'apple-touch-icon.png',
-      ],
-      manifest: {
-        name: 'vuerd',
-        short_name: 'vuerd',
-        description: 'erd editor',
-        theme_color: '#282c34',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
+      }),
+      VitePWA({
+        includeAssets: [
+          'favicon.svg',
+          'favicon.ico',
+          'robots.txt',
+          'apple-touch-icon.png',
         ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn-jsdelivr-net',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 7,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
+        manifest: {
+          name: 'vuerd',
+          short_name: 'vuerd',
+          description: 'erd editor',
+          theme_color: '#282c34',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'cdn-jsdelivr-net',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 7,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
               },
             },
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365,
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
+            {
+              urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'google-fonts-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
               },
             },
-          },
-        ],
-      },
-    }),
-  ],
-  server: {
-    open: true,
-  },
-});
+          ],
+        },
+      }),
+    ],
+  });
+};
