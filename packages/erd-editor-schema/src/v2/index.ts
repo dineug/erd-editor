df03dbdf3e00736bd371033aa51cab90fa67580e
dineug <dineug2@gmail.com>
2023-08-16 09:20:49 +0900

    fix: tsconfig.json

--- packages/erd-editor-schema/src/v2/index.ts
+++ packages/erd-editor-schema/src/v2/index.ts
@@ -1,2 +1,2 @@
 export { parser as schemaV2Parser } from '@/v2/parser';
-export { ERDEditorSchemaV2, SchemaV2Constants } from '@/v2/schema';
+export { type ERDEditorSchemaV2, SchemaV2Constants } from '@/v2/schema';
--- packages/erd-editor-schema/src/v3/index.ts
+++ packages/erd-editor-schema/src/v3/index.ts
@@ -1,2 +1,2 @@
 export { parser as schemaV3Parser } from '@/v3/parser';
-export { ERDEditorSchemaV3, SchemaV3Constants } from '@/v3/schema';
+export { type ERDEditorSchemaV3, SchemaV3Constants } from '@/v3/schema';
