414ee7347eec06922fcbf9cee85a2359bd42dde5
dineug <dineug2@gmail.com>
2023-08-31 01:13:42 +0900

    feat: store hook  startRelationship

---
+++ packages/erd-editor/src/engine/modules/relationship/hooks.ts
@@ -0,0 +1,97 @@
+import { SchemaV3Constants } from '@dineug/erd-editor-schema';
+import { throttle } from '@dineug/go';
+
+import type { CO, Hook } from '@/engine/hooks';
+import {
+  changeColumnNotNullAction,
+  changeColumnPrimaryKeyAction,
+  removeColumnAction,
+} from '@/engine/modules/tableColumn/atom.actions';
+import { query } from '@/utils/collection/query';
+
+const identification: CO = function* (channel, { doc, collections }) {
+  yield throttle(
+    channel,
+    () => {
+      const collection = query(collections).collection('relationshipEntities');
+      const relationships = collection.selectByIds(doc.relationshipIds);
+
+      for (const { id, end, identification } of relationships) {
+        const table = query(collections)
+          .collection('tableEntities')
+          .selectById(end.tableId);
+        if (!table) continue;
+
+        const columns = query(collections)
+          .collection('tableColumnEntities')
+          .selectByIds(end.columnIds)
+          .filter(column => table.columnIds.includes(column.id));
+        if (!columns.length) continue;
+
+        const value = columns.every(
+          column => column.options & SchemaV3Constants.ColumnOption.primaryKey
+        );
+
+        if (value === identification) {
+          continue;
+        }
+
+        collection.updateOne(id, relationship => {
+          relationship.identification = value;
+        });
+      }
+    },
+    500,
+    {
+      leading: true,
+      trailing: true,
+    }
+  );
+};
+
+const startRelationship: CO = function* (channel, { doc, collections }) {
+  yield throttle(
+    channel,
+    () => {
+      const collection = query(collections).collection('relationshipEntities');
+      const relationships = collection.selectByIds(doc.relationshipIds);
+
+      for (const { id, end, startRelationshipType } of relationships) {
+        const table = query(collections)
+          .collection('tableEntities')
+          .selectById(end.tableId);
+        if (!table) continue;
+
+        const columns = query(collections)
+          .collection('tableColumnEntities')
+          .selectByIds(end.columnIds)
+          .filter(column => table.columnIds.includes(column.id));
+        if (!columns.length) continue;
+
+        const value = columns.every(
+          column => column.options & SchemaV3Constants.ColumnOption.notNull
+        )
+          ? SchemaV3Constants.StartRelationshipType.dash
+          : SchemaV3Constants.StartRelationshipType.ring;
+
+        if (value === startRelationshipType) {
+          continue;
+        }
+
+        collection.updateOne(id, relationship => {
+          relationship.startRelationshipType = value;
+        });
+      }
+    },
+    500,
+    {
+      leading: true,
+      trailing: true,
+    }
+  );
+};
+
+export const hooks: Hook[] = [
+  [[removeColumnAction, changeColumnPrimaryKeyAction], identification],
+  [[removeColumnAction, changeColumnNotNullAction], startRelationship],
+];
--- packages/erd-editor/src/engine/store.hooks.ts
+++ packages/erd-editor/src/engine/store.hooks.ts
@@ -1,8 +1,7 @@
 import { cancel, type Channel, channel, go, put } from '@dineug/go';
 import { type AnyAction } from '@dineug/r-html';
-import { isString } from '@dineug/shared';
 
-import { hooks as tableColumnHooks } from '@/engine/modules/tableColumn/hooks';
+import { hooks as relationshipHooks } from '@/engine/modules/relationship/hooks';
 import type { Store } from '@/engine/store';
 
 type Task = {
@@ -11,14 +10,14 @@ type Task = {
   proc: Promise<any>;
 };
 
-const hooks = [...tableColumnHooks];
+const hooks = [...relationshipHooks];
 
 export function createHooks(store: Store) {
   const tasks: Task[] = hooks.map(([pattern, hook]) => {
     const ch = channel();
 
     return {
-      pattern: pattern.map(v => (isString(v) ? v : v.type)),
+      pattern: pattern.map(String),
       channel: ch,
       proc: go(hook, ch, store.state, store.context),
     };
